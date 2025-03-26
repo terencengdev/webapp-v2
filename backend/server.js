b0ceeb159a964d096cf40baa7cb60543fccb0e74
terencengdev <terencejiahao@gmail.com>
2025-03-26 23:16:03 +0800

    Update gitignore and added env value

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   port: 3306,
 });
 
